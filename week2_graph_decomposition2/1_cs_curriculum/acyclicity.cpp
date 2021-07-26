15036186d0695c8a559165d4d5d147c9e4566df3
Carlos Daniel <carlosdaniel.londono@utp.edu.co>
2021-07-25 19:40:17 -0500

    My version

--- week2_graph_decomposition2/1_cs_curriculum/acyclicity.cpp
+++ week2_graph_decomposition2/1_cs_curriculum/acyclicity.cpp
@@ -4,19 +4,59 @@
 using std::vector;
 using std::pair;
 
-int acyclic(vector<vector<int> > &adj) {
-  //write your code here
-  return 0;
+typedef vector<pair<pair<bool, unsigned int>, vector<unsigned int>>> graph;
+typedef pair<pair<bool, unsigned int>, vector<unsigned int>> vertex;
+
+unsigned int order;
+short isCyclic = 0;
+
+// Visited(vertex)
+bool visited(vertex &v) {
+    return v.first.first;
+}
+
+// Explore(vertex)
+void explore(vertex &v, graph &g) {
+    v.first.first = true;
+    v.first.second = order;
+    order++;
+    // for each vertex reachable from vertex where we are
+    unsigned int length = v.second.size();
+    for (unsigned int i = 0; i < length; i++){
+        if (!visited(g[v.second[i]])) {
+            explore(g[v.second[i]], g);
+        } else {
+            if (g[v.second[i]].first.second <= v.first.second) {
+                isCyclic = 1;
+            }
+        }
+    }
+}
+
+// DFS(Graph)
+void dfs(graph &g) {
+    // for each vertex from graph
+    unsigned int length = g.size();
+    for (unsigned int i = 0; i < length; i++) {
+        order = 1;
+        if (!visited(g[i])) {
+            explore(g[i], g);
+        }
+    }
 }
 
 int main() {
-  size_t n, m;
+  int n, m;
   std::cin >> n >> m;
-  vector<vector<int> > adj(n, vector<int>());
-  for (size_t i = 0; i < m; i++) {
-    int x, y;
+  //Vector of pairs (pair<bool, unsigned int>, Vector) -> DIRECTED GRAPH
+  graph g(n, pair<pair<bool, unsigned int>, vector<unsigned int>>());
+  for (int i = 0; i < m; i++) {
+    unsigned int x, y;
     std::cin >> x >> y;
-    adj[x - 1].push_back(y - 1);
+    // Mark as unvisited
+    g[x - 1].first.first = false;
+    g[x - 1].second.push_back(y - 1);
   }
-  std::cout << acyclic(adj);
+  dfs(g);
+  std::cout << isCyclic;
 }
