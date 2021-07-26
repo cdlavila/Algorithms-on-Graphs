e4a76dcff3d8640de898917cd93b610d8c41b9f6
Carlos Daniel <carlosdaniel.londono@utp.edu.co>
2021-07-25 22:45:07 -0500

    Al fin

--- week2_graph_decomposition2/1_cs_curriculum/acyclicity.cpp
+++ week2_graph_decomposition2/1_cs_curriculum/acyclicity.cpp
@@ -4,58 +4,36 @@
 using std::vector;
 using std::pair;
 
-typedef vector<pair<bool, vector<unsigned int>>> graph;
-typedef pair<bool, vector<unsigned int>> vertex;
-
-short isCyclic = 0;
-
-// Visited(vertex)
-bool visited(vertex &v) {
-    return v.first;
-}
-
-// Explore(vertex)
-vertex explore(vertex &v, graph &g) {
-    v.first = true;
-    vertex end;
-    // for each vertex reachable from vertex where we are
-    unsigned int length = v.second.size();
-    for (unsigned int i = 0; i < length; i++){
-        if (!visited(g[v.second[i]])) {
-            end = explore(g[v.second[i]], g);
+short acyclic(vector<pair<bool, vector<unsigned int>>> &adj, int node) {
+    short answer = 0;
+    // Visited
+    adj[node].first = true;
+    unsigned int limit = adj[node].second.size();
+    for (int i = 0; i < limit; i++) {
+        int next_node = adj[node].second[i];
+        //If it hasn't already been visited
+        if (!adj[next_node].first) {
+            answer = acyclic(adj, next_node);
         } else {
-            return g[v.second[i]];
+            return 1;
         }
     }
-    if (end == v) {
-        isCyclic = 1;
-    }
-    return end;
-}
-
-// DFS(Graph)
-void dfs(graph &g) {
-    // for each vertex from graph
-    unsigned int length = g.size();
-    for (unsigned int i = 0; i < length; i++) {
-        if (!visited(g[i])) {
-            explore(g[i], g);
-        }
-    }
+    return answer;
 }
 
 int main() {
-    int n, m;
-    std::cin >> n >> m;
-    //Vector of pairs (pair<bool, unsigned int>, Vector) -> DIRECTED GRAPH
-    graph g(n, pair<bool, vector<unsigned int>>());
-    for (int i = 0; i < m; i++) {
-        unsigned int x, y;
-        std::cin >> x >> y;
-        // Mark as unvisited
-        g[x - 1].first = false;
-        g[x - 1].second.push_back(y - 1);
-    }
-    dfs(g);
-    std::cout << isCyclic;
+  int n, m;
+  std::cin >> n >> m;
+  int initial;
+  //Vector of pairs (bool, Vector) -> DIRECTED GRAPH
+  vector<pair<bool, vector<unsigned int>>> adj(n, pair<bool, vector<unsigned int>>());
+  for (int i = 0; i < m; i++) {
+    int x, y;
+    std::cin >> x >> y;
+    // Mark as unvisited
+    adj[x - 1].first = false;
+    adj[x - 1].second.push_back(y - 1);
+    initial = x - 1;
+  }
+  std::cout << acyclic(adj, initial);
 }
